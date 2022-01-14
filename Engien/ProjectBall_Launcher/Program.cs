4611a9a6988110cb1bcbaa573ce69c25c8bd935a
RCx02 <93350632+RCx02@users.noreply.github.com>
2022-01-14 23:28:37 +0000

    Import

--- Engien/ProjectBall_Launcher/Program.cs
+++ Engien/ProjectBall_Launcher/Program.cs
@@ -8,9 +8,6 @@ namespace ProjectBall_Launcher
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
